77c8c484c4b70bfaa2758166bc2ce93a13f60df2
sahilhirve23 <128059019+sahilhirve23@users.noreply.github.com>
2025-11-12 14:32:58 +0530

    Update Loader import path to include file extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Loader from "./components/Loader";
+import Loader from "./components/Loader.jsx";
 import React, { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 import gsap from 'gsap';
