0d85969c3cd63b86e9c15478fc07c070762ebe77
sahilhirve23 <128059019+sahilhirve23@users.noreply.github.com>
2025-11-12 19:34:03 +0530

    Refactor main.jsx to include loading state

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import Loader from "./components/Loader";
+import App from "./App";
+
+function Root() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return isLoading ? (
+    <Loader onComplete={() => setIsLoading(false)} />
+  ) : (
+    <App />
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
